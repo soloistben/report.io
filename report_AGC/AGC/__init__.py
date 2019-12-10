e00457c9d8827ca8f55207503ae1f3d4272dea1d
soloistben <fbi9111111@gmail.com>
2019-12-10 15:37:27 +0800

    del old file

--- report_AGC/AGC/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function		#就算用python2，print也需要括号
-from __future__ import division				#精确除法，3/4=0.75 不会等于0
-
-from .layers import *
-from .models import *
-from .utils import *
